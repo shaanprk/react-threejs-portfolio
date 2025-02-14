62baa238a42efd3365e3751d95700984aa7d9cf3
Sean Park <jungbeanshaanprk@gmail.com>
2025-02-14 09:42:03 -0800

    Added onto the HTML structure

--- react/src/pages/About/AboutPage.jsx
+++ react/src/pages/About/AboutPage.jsx
@@ -1,11 +1,15 @@
 import styles from './AboutPage.module.css'
 
+import seal from '../../assets/images/seal.png'
+import signature from '../../assets/images/signature_vertical.png'
+
 const AboutPage = () => {
   return (
     <div className={styles.aboutPage}>
       <div className={styles.background}>
         <div className={styles.content}>
           <h1>About Page</h1>
+          <a href="https://binni.dev" target="_blank" rel="noopener noreferrer">Click Here</a>
           <p>Hi</p>
           <br />
           <p>Hi</p>
@@ -129,6 +133,38 @@ const AboutPage = () => {
           <p>Hi</p>
           <p>Hi</p>
           <p>Hi</p>
+          <br />
+          <p>Hi</p>
+          <p>Hi</p>
+          <p>Hi</p>
+          <br />
+          <p>Hi</p>
+          <p>Hi</p>
+          <p>Hi</p>
+          <br />
+          <p>Hi</p>
+          <p>Hi</p>
+          <p>Hi</p>
+          <br />
+          <p>Hi</p>
+          <p>Hi</p>
+          <p>Hi</p>
+          <br />
+          <p>Hi</p>
+          <p>Hi</p>
+          <p>Hi</p>
+          <div className={styles.dojang}>
+            <img 
+              src={signature} 
+              alt="Signature" 
+              title="Signature"
+              className={styles.signature} />
+            <img 
+              src={seal} 
+              alt="Seal" 
+              title="Signature"
+              className={styles.seal} />
+          </div>
         </div>
       </div>
     </div>
